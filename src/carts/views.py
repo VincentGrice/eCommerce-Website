2aa241982f52a7eab9a54dd4167d0a4312e5cb95
Vincent Grice <39641488+vgrice888@users.noreply.github.com>
2018-12-08 16:28:55 -0500

    completed checkout with thank you note

--- src/carts/views.py
+++ src/carts/views.py
@@ -47,31 +47,32 @@ def checkout_home(request):
 	guest_form = GuestForm()
 	address_form = AddressForm()
 
-	billling_address_id = request.session.get("billling_address_id", None)
+	billing_address_id = request.session.get("billing_address_id", None)
 	shipping_address_id = request.session.get("shipping_address_id", None)
 
 	billing_profile, billing_profile_created = BillingProfile.objects.new_or_get(request)
 	address_qs = None
 	if billing_profile is not None:
-		address_qs = Address.objects.filter(billing_profile=billing_profile)
+		if request.user.is_authenticated():
+			address_qs = Address.objects.filter(billing_profile=billing_profile)
 		order_obj, order_obj_created = Order.objects.new_or_get(billing_profile, cart_obj)
 		if shipping_address_id:
 			order_obj.shipping_address = Address.objects.get(id=shipping_address_id)
 			del request.session["shipping_address_id"]
-		if billling_address_id:
-			order_obj.billling_address = Address.objects.get(id=billing_address_id)
+		if billing_address_id:
+			order_obj.billing_address = Address.objects.get(id=billing_address_id)
 			del request.session["billing_address_id"]
-		if billling_address_id or shipping_address_id:
+		if billing_address_id or shipping_address_id:
 			order_obj.save()
 
 	if request.method == "POST":
-		"some check that order is done"
+		"check that order is done"
 		is_done = order_obj.check_done()
 		if is_done:
 			order_obj.mark_paid()
 			request.session['cart_items'] = 0
 			del request.session['cart_id']
-			return redirect("/cart/success")
+			return redirect("cart:success")
 
 	context = {
 		"object": order_obj,
@@ -82,3 +83,8 @@ def checkout_home(request):
 		"address_qs": address_qs,
 	}
 	return render(request, "carts/checkout.html", context)
+
+
+
+def checkout_done_view(request):
+	return render(request, "carts/checkout-done.html", {})
