751ac8418f34a12578539e0d55897311a483f399
Vincent Grice <39641488+vgrice888@users.noreply.github.com>
2018-12-13 23:11:28 -0500

    create view for payment method with stripe

--- src/billing/views.py
+++ src/billing/views.py
@@ -4,3 +4,11 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 # Create your views here.
+import stripe
+stripe.api_key = ""
+STRIPE_PUB_KEY = ''
+
+def payment_method_view(request):
+	if request.method == 'POST':
+		print(request.POST)
+	return render(request, 'billing/payment-method.html', {"publish_key":STRIPE_PUB_KEY})
